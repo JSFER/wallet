493674b91c79ff65a02fef9dff0880b7103217ca
xuejian.xu <jearbilove@yeah.net>
2018-08-19 23:35:13 +0800

    update

--- src/container/AdvHost/AdvHost.jsx
+++ src/container/AdvHost/AdvHost.jsx
@@ -1,13 +1,19 @@
+import './AdvHost.css'
 import React from 'react'
 
 // components
 import Header from '@src/component/Header/Header'
+import Footer from '@src/component/Footer/Footer'
 
 export default class AdvHost extends React.PureComponent{
     render(){
         return (
             <div id="AdvHost">
                 <Header />
+                <div className="g-page">
+                    <div className="banner"></div>
+                </div>
+                <Footer />
             </div>
         )
     }
